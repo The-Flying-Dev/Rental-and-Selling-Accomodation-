6c6d54cb82784f54f8180fda22914d52c79905fc
The-Flying-Dev <leandranm@outlook.com>
2022-10-01 00:30:38 -0700

    update explore page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import UnauthorizedRoute from './components/UnauthorizedRoute';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
+import Category from './pages/Category';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -18,7 +19,8 @@ function App() {
       <Navbar />  
       <Routes>
         <Route path="/" element={<Explore />} />
-        <Route path="/offers" element={<Offers />} />   
+        <Route path="/offers" element={<Offers />} />
+        <Route path='/category/:categoryName' element={<Category />} />
         <Route path="/profile" element={<UnauthorizedRoute />}>
           <Route path="/profile" element={<Profile />} />
         </Route>      
--- src/components/UnauthorizedRoute.js
+++ src/components/UnauthorizedRoute.js
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStatus } from "../hooks/useAuthStatus";
 import Spinner from "./Spinner";
 
-// Outlet is profile page if logged in
+// Outlet is profile page if user is logged in otherwise user will be directed to sign-in page
 
 const UnauthorizedRoute = () => {
 
