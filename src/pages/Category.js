29a058e21d4d694b2d5cdb941c86713c6a12ae82
The-Flying-Dev <leandranm@outlook.com>
2022-10-02 07:32:32 -0700

    Listing Item

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,7 +1,84 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { collection, getDocs, query, where, orderBy, limit } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import Spinnner from '../components/Spinner';
+import ListingItem from '../components/ListingItem'; 
+import { toast } from 'react-toastify';
+
 
 export default function Category() {
+
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference 
+        const listingsRef = collection(db, 'listings')
+
+        // Create a query 
+        const q = query(
+          listingsRef,
+          where('type', '==', params.categoryName), // refers to rent or sale
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // Execute query
+        const querySnap = await getDocs(q);
+
+        const listings = [];
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,             //manually add id, since the collection does not have id column
+            data: doc.data(),
+          })
+        })
+
+        setListings(listings)
+        setLoading(false)
+      } catch (error) {
+        toast.error('unable to fetch listings')
+      }
+    }
+
+    fetchListings();
+  }, [params.categoryName])
+  
   return (
-    <div>Category</div>
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}
+        </p>
+      </header>
+
+      {loading ? (
+        <Spinnner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <ListingItem 
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are no listings</p>
+      )}
+    </div>
   )
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom';
 import { getAuth, updateProfile } from 'firebase/auth'
 import { updateDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase.config';
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify';
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
 
 
 export default function Profile() {
@@ -108,6 +110,12 @@ export default function Profile() {
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='home' />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt='arrow right' />
+        </Link>
       </main>
     </div>
   );
