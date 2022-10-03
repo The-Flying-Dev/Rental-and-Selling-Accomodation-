f5fedad790cea0edf4d8e5190434eef2f249da42
The-Flying-Dev <leandranm@outlook.com>
2022-10-02 23:56:32 -0700

    Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams, useSearchParams } from 'react-router-dom';
+import { doc, getDoc } from './firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
 
 export default function Contact() {
+
+  // initailize state
+  const [message, setMessage] = useState('');
+  const [landlord, setLandlord] = useState(null);
+  // eslint-disable-next-line
+  const [searchParams, setSearchParams] = useSearchParams();
+
+
+  // initialize hook
+  const params = useParams();
+
+  // gets users id from users collection in firebase db   
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, 'users', params.landlordId) 
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data())
+      } else {
+        toast.error('Unable to get landlord data')
+      }
+    }
+
+    // calling async function
+    getLandlord() 
+  }, [params.landlordId])
+
+  const onChange = (e) => setMessage(e.target.value)
+
   return (
-    <div>Contact</div>
+    <div className='pageContainer'>
+      <header>
+        <p className='pageHeader'>Contact Landlord</p>
+      </header>
+
+      {/* if item exists the display data */}
+      {landlord !== null && (
+        <main>
+          <div className='contactLandlord'>
+            <p className='landlordName'>Contact {landlord?.name}</p>
+          </div>
+
+          <form className='messageForm'>
+            <div className='messageDiv'>
+              <label htmlFor='message' className='messageLabel'>
+                Message
+              </label>
+              <textarea
+                name='message'
+                id='message'
+                className='textarea'
+                value={message}
+                onChange={onChange}
+              ></textarea>
+            </div>
+
+            { /* opens up default email app to send email */}
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                'listingName'
+              )}&body=${message}`}
+            >
+              <button type='button' className='primaryButton'>
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
+      )}
+    </div>
   )
 }
