c97aade3240d7b7d32c5788f641a652841ccd66a
The-Flying-Dev <leandranm@outlook.com>
2022-10-03 00:55:01 -0700

    Slider async/await

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,86 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+import Spinner from './Spinner'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 export default function Slider() {
+
+  // initialize
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
+
+  const navigate = useNavigate()
+
+  // grabs listings from firebase, then order by query conditions
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listingsRef = collection(db, 'listings')
+      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5))
+      const querySnap = await getDocs(q)
+
+      let listings = []
+
+
+      // loops through listing doc and adds it to listings array initialized above
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      // updating state
+      setListings(listings)
+      setLoading(false)
+    }
+
+    // calling async function
+    fetchListings()
+  }, [])
+
+  if (loading) {
+    return <Spinner />
+  }
+
+  if (listings.length === 0) {
+    return<></>
+  }
+
+  // loop through the destructured listings object and passes each item to SwiperSlide component
+  // with a clickable link to the item
   return (
-    <div>Slider</div>
+    listings && (
+      <>
+        <p className='exploreHeading'>Recommended</p>
+
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                style={{
+                  background: `url(${data.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='swiperSlideDiv'
+              >
+                <p className='swiperSlideText'>{data.name}</p>
+                <p className='swiperSlidePrice'>
+                  ${data.discountedPrice ?? data.regularPrice}{' '}
+                  {data.type === 'rent' && '/ month'}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
   )
 }
